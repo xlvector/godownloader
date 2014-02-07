6995a88c1b2b47f57ab03cd5d7174bede7b33143
Liang Xiang <xlvector@gmail.com>
2014-02-07 22:22:04 +0800

    use rule matcher

---
+++ rule_matcher.go
@@ -0,0 +1,62 @@
+package downloader
+
+import (
+	"regexp"
+	"strings"
+)
+
+type Rule struct {
+	Regex    *regexp.Regexp
+	Priority int
+}
+
+type RuleList []Rule
+
+type RuleMatcher struct {
+	SiteRules   map[string]RuleList
+	CommonRules RuleList
+}
+
+func NewRuleMatcher() *RuleMatcher {
+	ret := RuleMatcher{}
+	ret.SiteRules = make(map[string]RuleList)
+	return &ret
+}
+
+func (self *RuleMatcher) AddRule(rule string, priority int) {
+	domain := ExtractMainDomain(rule)
+	pattern := regexp.MustCompile(rule)
+	if strings.Contains(domain, "*") || strings.Contains(domain, "[") {
+		self.CommonRules = append(self.CommonRules, Rule{Regex: pattern, Priority: priority})
+	} else {
+		_, ok := self.SiteRules[domain]
+		if !ok {
+			self.SiteRules[domain] = []Rule{}
+		}
+		self.SiteRules[domain] = append(self.SiteRules[domain], Rule{Regex: pattern, Priority: priority})
+	}
+}
+
+func (self *RuleMatcher) MatchRule(link string) int {
+	domain := ExtractMainDomain(link)
+	rules, ok := self.SiteRules[domain]
+	maxPriority := 0
+	if ok {
+		for _, rule := range rules {
+			if rule.Regex.FindString(link) == link {
+				if maxPriority < rule.Priority {
+					maxPriority = rule.Priority
+				}
+			}
+		}
+	} else {
+		for _, rule := range self.CommonRules {
+			if rule.Regex.FindString(link) == link {
+				if maxPriority < rule.Priority {
+					maxPriority = rule.Priority
+				}
+			}
+		}
+	}
+	return maxPriority
+}
---
+++ rule_matcher_test.go
@@ -0,0 +1,24 @@
+package downloader
+
+import (
+	"testing"
+)
+
+func TestRuleMatcher(t *testing.T) {
+	rm := NewRuleMatcher()
+
+	rm.AddRule("http://[a-z0-9]+.sina.com.cn/", 2)
+	rm.AddRule("http://[a-z0-9]+.[a-z0-9]+.com/", 1)
+
+	if rm.MatchRule("http://t.sina.com.cn/") != 2 {
+		t.Error()
+	}
+
+	if rm.MatchRule("http://www.ustc.com/") != 1 {
+		t.Error()
+	}
+
+	if rm.MatchRule("http://www.sina.com.cn/hello") != 0 {
+		t.Error()
+	}
+}
