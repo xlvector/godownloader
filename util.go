7f429a58f9c63dea55500f47429a83c15ed5bdd9
Liang Xiang <xlvector@gmail.com>
2014-05-07 15:11:06 +0800

    change baidu url

--- util.go
+++ util.go
@@ -21,9 +21,6 @@ func GetHostName() string {
 func extractUrlParams(link string) map[string]string{
 	tks := strings.Split(link, "?")
 	if len(tks) != 2 {
-		tks = strings.Split(link, "#")
-	}
-	if len(tks) != 2 {
 		return nil
 	}
 	params := strings.Split(tks[1], "&")
